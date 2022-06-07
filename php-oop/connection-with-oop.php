a11154cdc7bb86aaac8cf04f466ea52441417363
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-07 09:15:55 +0200

    modified db connection  with php

--- php-oop/connection-with-oop.php
+++ php-oop/connection-with-oop.php
@@ -2,6 +2,12 @@
 
 //you can connect to the db with the following code:
 
-$connection=new mysqli("localhost","root","","rwanda");
+$connection=new mysqli("localhost","root","","admin_entities");
+
+if($connection->connect_error){
+    die("connection failed: ".$connection->connect_error);
+}else{
+    echo "connected successfully";
+}
 
 ?>
